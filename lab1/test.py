31802a2da271f84e9e999dc5d14d3be34ccf5554
Yeldanam <muratbekeldana16>
2024-01-19 10:29:06 +0600

    Lab1

---
+++ lab1/test.py
@@ -0,0 +1,49 @@
+a = 3
+a = "3"
+aStr = str(3)
+
+floatB = float ("3")
+print (floatB)
+
+floatb= 2.0
+print(floatB)
+print(floatb)
+
+floatLongName = 3.2
+float_long_name = 3.3
+FloatLongName = 3.4
+
+newArray = [1,2,3]
+
+var1, var2, var3 = newArray
+var1 = var2 = var3 = "4"
+
+print(var1, var2, var3)
+print(var1 + var2 + var3)
+
+var4 = 4
+var5 = 5
+print(var4, var5)
+
+a = 3 + 1j
+b = 4 + 2j
+
+strVal = "String"
+
+print(type(strVal))
+print(strVal[0:2])
+print(strVal[1])
+
+
+for strChar in strVal:
+    print(strChar)
+
+print(strChar)
+print(len(strChar))
+
+if "some" in "some extra one":
+    print("YES")
+    x = 1
+else:
+    print("NO")
+
