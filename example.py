f821cc4e71611900bc44252cfc7ef41627e76d06
Yeldanam <157086293+Yeldanam@users.noreply.github.com>
2024-01-26 09:50:18 +0600

    Add files via upload

--- example.py
+++ example.py
@@ -1,272 +1,272 @@
-# python get started
-print("Hello, World!")
+#task 1
+print("Hello, World!")  #output:Hello,world!
 
 
-#  python syntax
+#task 2
 if 5 > 2:
-  print("Five is greater than two!")
+  print("Five is greater than two!") 
 
-
-#  python comments
+#task 3
 #This is a comment
 
-
-#  python variables
+#task 4
 x = 5
 y = "John"
+print(x)    #output:5
+print(y)    #John
+
+#task 5
+x = 4       
+x = "Sally" #output:Sally
 print(x)
-print(y)
+ 
 
-x = 4       # x is of type int
-x = "Sally" # x is now of type str
-print(x)
-
-x = str(3)    # x will be '3'
-y = int(3)    # y will be 3
-z = float(3)  # z will be 3.0
-
+#task 6
 x = 5
 y = "John"
-print(type(x))
-print(type(y))
-
-x = "John"
-# is the same as
-x = 'John'
-
-a = 4
-A = "Sally"
-#A will not overwrite a
+print(type(x)) #output:int
+print(type(y)) #output:str
 
 
-#  python variable names
-myvar = "John"
-my_var = "John"
-_my_var = "John"
-myVar = "John"
-MYVAR = "John"
-myvar2 = "John"
-
-myVariableName = "John"
-
-MyVariableName = "John"
-
-my_variable_name = "John"
-
-
-#  python Multiple Values
+#task 7
 x, y, z = "Orange", "Banana", "Cherry"
-print(x)
+print(x)    
 print(y)
-print(z)
+print(z) 
+#output:
+#Orange
+#Banana
+#Cherry
 
+#task 8
 x = y = z = "Orange"
 print(x)
 print(y)
 print(z)
+#output:
+#Orange
+#Orange
+#Orange
 
+#task 9
 fruits = ["apple", "banana", "cherry"]
 x, y, z = fruits
-print(x)
-print(y)
-print(z)
+print(x)        #apple
+print(y)        #banana
+print(z)        #cherry
 
-#python Output Variables
+#task 10
 x = "Python is awesome"
-print(x)
+print(x)        #Python is awesome
 
+#task 11
 x = "Python"
 y = "is"
 z = "awesome"
-print(x, y, z)
+print(x, y, z) #Python is awesome
 
+#task 12
 x = "Python "
 y = "is "
 z = "awesome"
-print(x + y + z)
+print(x + y + z) #Python is awesome
 
+#task 13
 x = 5
 y = 10
-print(x + y)
+print(x + y) #15
 
+#task 14
 x = 5
 y = "John"
-print(x, y)
+print(x, y)  #5,John
 
-#python Global Variables
+#task 15
 x = "awesome"
 def myfunc():
   print("Python is " + x)
 
-myfunc()
+myfunc()      #Python is awesome
+myfunc()      
 
 
-
+#task 16
 x = "awesome"
 def myfunc():
   x = "fantastic"
   print("Python is " + x)
 myfunc()
 print("Python is " + x)
+#Python is fantastic
+#Python is awesome
 
-
+#task 17
 def myfunc():
   global x
   x = "fantastic"
-
 myfunc()
-
 print("Python is " + x)
+#Python is fantastic
 
-
+#task 18
 x = "awesome"
-
 def myfunc():
   global x
   x = "fantastic"
-
 myfunc()
-
 print("Python is " + x)
+#Python is fantastic
 
 
-#python Data Types
+#task 19
 x = 5
 print(type(x))
+#int
 
-x = "Hello World"   #	str	
-x = 20	           #int	   
-x = 20.5	#float	
-x = 1j	#complex	
-x = ["apple", "banana", "cherry"]	#list	
-x = ("apple", "banana", "cherry")	#tuple	
-x = range(6)	#range	
-x = {"name" : "John", "age" : 36}	#dict	
-x = {"apple", "banana", "cherry"}	#set	
-x = frozenset({"apple", "banana", "cherry"})	#frozenset	
-x = True	#bool	
-x = b"Hello"	#bytes	
-x = bytearray(5)	#bytearray	
-x = memoryview(bytes(5))	#memoryview	
-x = None	#NoneType
-
-
-#python Numbers
-x = 1    # int
-y = 2.8  # float
-z = 1j   # complex
-
+#task 20
 x = 1
 y = 35656222554887711
 z = -3255522
-
 print(type(x))
 print(type(y))
 print(type(z))
+#int
+#int
+#int
 
+#task 21
 x = 1.10
 y = 1.0
 z = -35.59
-
 print(type(x))
 print(type(y))
 print(type(z))
+#float
+#float
+#float
 
+#task 22
 x = 35e3
 y = 12E4
 z = -87.7e100
-
 print(type(x))
 print(type(y))
 print(type(z))
+#float
+#float
+#float
 
-
-#python Casting
-x = int(1)   # x will be 1
-y = int(2.8) # y will be 2
-z = int("3") # z will be 3
-
-x = float(1)     # x will be 1.0
-y = float(2.8)   # y will be 2.8
-z = float("3")   # z will be 3.0
-w = float("4.2") # w will be 4.2
-
-x = str("s1") # x will be 's1'
-y = str(2)    # y will be '2'
-z = str(3.0)  # z will be '3.0'
-
-#python Strings
+#task 23
 print("Hello")
 print('Hello')
+#Hello
+#Hello
 
+#task 24
 a = "Hello"
 print(a)
+#Hello
 
+#task 25
 a = """Lorem ipsum dolor sit amet,
 consectetur adipiscing elit,
 sed do eiusmod tempor incididunt
 ut labore et dolore magna aliqua."""
 print(a)
+#Lorem ipsum dolor sit amet,
+#consectetur adipiscing elit,
+#sed do eiusmod tempor incididunt
+#ut labore et dolore magna aliqua.
 
+
+#task 26
 a = "Hello, World!"
 print(a[1])
+#e
 
+#task 27
 for x in "banana":
   print(x)
+#n
+#a
+#n
+#a
 
+#task 28
 a = "Hello, World!"
 print(len(a))
+#13
 
+#task 29
 txt = "The best things in life are free!"
 print("free" in txt)
+#True
+
+#task 30
 txt = "The best things in life are free!"
 if "free" in txt:
   print("Yes, 'free' is present.")
+  #Yes, 'free' is present.
 
+#task 31
 txt = "The best things in life are free!"
 print("expensive" not in txt)
+#True
 
+#task 32
 txt = "The best things in life are free!"
 if "expensive" not in txt:
   print("No, 'expensive' is NOT present.")
+#No, 'expensive' is NOT present.
 
-
-#python - Slicing Strings
+#task 33
 b = "Hello, World!"
 print(b[2:5])
+#llo
 
+#task 34
 b = "Hello, World!"
 print(b[:5])
+#Hello
 
+#task 35
 b = "Hello, World!"
 print(b[2:])
+#llo, World!
 
+#task 36
 b = "Hello, World!"
 print(b[-5:-2])
+#orl
 
-
-#python - Modify Strings
+#task 37
 a = "Hello, World!"
 print(a.upper())
+#HELLO, WORLD!
 
+#task 38
 a = "Hello, World!"
 print(a.lower())
+#Hello, World!
 
-
+#task 39
 a = " Hello, World! "
-print(a.strip()) # returns "Hello, World!"
+print(a.strip()) 
+#Hello, World! 
 
+
+#task 40
 a = "Hello, World!"
 print(a.replace("H", "J"))
+#Jello, World!
 
+#task 41
 a = "Hello, World!"
-print(a.split(",")) # returns ['Hello', ' World!']
+print(a.split(",")) 
+#['Hello', ' World!']
 
-#python Booleans
-print(10 > 9)
-print(10 == 9)
-print(10 < 9)
 
+#task 42
 a = 200
 b = 33
 
@@ -274,141 +274,133 @@ if b > a:
   print("b is greater than a")
 else:
   print("b is not greater than a")
+#b is not greater than a
 
 
+#task 43
 print(bool("Hello"))
 print(bool(15))
+#True
+#True
 
+#task 44
 x = "Hello"
 y = 15
-
 print(bool(x))
 print(bool(y))
+#True
+#True
 
-bool("abc")
-bool(123)
-bool(["apple", "cherry", "banana"])
-
-bool(False)
-bool(None)
-bool(0)
-bool("")
-bool(())
-bool([])
-bool({})
-
-
+#task 45
 class myclass():
   def __len__(self):
     return 0
-
 myobj = myclass()
 print(bool(myobj))
+#False
 
-
+#task 46
 def myFunction() :
   return True
-
 print(myFunction())
+#True
 
-
+#task 47
 def myFunction() :
   return True
-
 if myFunction():
   print("YES!")
 else:
   print("NO!")
+#YES!
 
-
-#Python Operators
+#task 48
 print(10 + 5)
+#15
 
-
-#Python Lists
-mylist = ["apple", "banana", "cherry"]
-
+#task 49
 thislist = ["apple", "banana", "cherry"]
 print(thislist)
+#['apple', 'banana', 'cherry']
 
+#task 50
 thislist = ["apple", "banana", "cherry", "apple", "cherry"]
 print(thislist)
+#['apple', 'banana', 'cherry', 'apple', 'cherry']
 
-
+#task 51
 thislist = ["apple", "banana", "cherry"]
 print(len(thislist))
+#3
 
 
-#Python Tuples
-mytuple = ("apple", "banana", "cherry")
-
+#task 52
 thistuple = ("apple", "banana", "cherry")
 print(thistuple)
+#('apple', 'banana', 'cherry')
 
+
+#task 53
 thistuple = ("apple", "banana", "cherry", "apple", "cherry")
 print(thistuple)
+#apple,banana,cherry,apple,cherry
 
-
+#task 54
 thistuple = ("apple", "banana", "cherry")
 print(len(thistuple))
+#3
 
+#task 55
 thistuple = ("apple",)
 print(type(thistuple))
+#tuple
 
-#NOT a tuple
+#task 56
 thistuple = ("apple")
 print(type(thistuple))
+#str
 
-#Python Sets
-myset = {"apple", "banana", "cherry"}
-
-
+#task 58
 thisset = {"apple", "banana", "cherry"}
 print(thisset)
+#cherry , apple , banana
 
-
-thisset = {"apple", "banana", "cherry", "apple"}
-
-print(thisset)
-
-#Python Dictionaries
-thisdict = {
-  "brand": "Ford",
-  "model": "Mustang",
-  "year": 1964
-}
-Diction
-
-
+#task 59
 thisdict = {
   "brand": "Ford",
   "model": "Mustang",
   "year": 1964
 }
 print(thisdict)
+#brand : Ford, model : Mustang , year : 1964
 
+#task 60
 thisdict = {
   "brand": "Ford",
   "model": "Mustang",
   "year": 1964
 }
 print(thisdict["brand"])
+#Ford
 
-
-#Python If ... Else
+#task 61
 a = 33
 b = 200
 if b > a:
   print("b is greater than a")
+#b is greater than a
 
+#task 62
 a = 33
 b = 33
 if b > a:
   print("b is greater than a")
 elif a == b:
   print("a and b are equal")
+#a and b are equal
 
-  a = 200
+#task 63
+a = 200
 b = 33
 if b > a:
   print("b is greater than a")
@@ -416,5 +408,5 @@ elif a == b:
   print("a and b are equal")
 else:
   print("a is greater than b")
-
+#a is greater than b
 
